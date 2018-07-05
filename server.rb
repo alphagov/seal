adacd440610529acb4865b6c578f4f0d2cfeedf0
Tatiana Stantonian <tatiana.stantonian@digital.cabinet-office.gov.uk>
2018-07-05 10:31:40 +0100

    Trifling with API endpoints

--- server.rb
+++ server.rb
@@ -17,14 +17,22 @@ class SealApp < Sinatra::Base
     end
   end
 
-  post '/add-member/:team_name/:team_member/:secret' do
+  post '/bark-quotes/:team_name' do
+    Seal.new(params[:team_name], "quotes").bark
+    "Seal received message with #{params[:team_name]} team name"
+  end
+
+  post '/slack-bark' do
+    slack_signing_secret = request.env["SLACK_SIGNING_SECRET"]
+    request_payload = JSON.parse(request.body.read)
+    "this is working - request_payload #{request_payload} - slack_signing_secret #{request.env["SLACK_SIGNING_SECRET"]}"
     if params[:secret] == ENV["SEAL_SECRET"]
       Seal.new(params[:team_name]).bark
       "Seal received message with #{params[:team_name]} team name"
     end
   end
 
-  post '/add-team/:team_name/:secret' do
+  post '/add-team' do
     if params[:secret] == ENV["SEAL_SECRET"]
       team = Team.create!(name: "#{params[:team_name]}")
       Seal.new(params[:team_name]).bark
@@ -32,8 +40,26 @@ class SealApp < Sinatra::Base
     end
   end
 
-  post '/bark-quotes/:team_name' do
-    Seal.new(params[:team_name], "quotes").bark
-    "Seal received message with #{params[:team_name]} team name"
+  post '/add-member' do
+    if params[:secret] == ENV["SEAL_SECRET"]
+      Seal.new(params[:team_name]).bark
+      "Seal received message with #{params[:team_name]} team name"
+    end
+  end
+
+  get '/settings' do
+    if params[:secret] == ENV["SEAL_SECRET"]
+      team = Team.create!(name: "#{params[:team_name]}")
+      Seal.new(params[:team_name]).bark
+      "Seal received message with #{params[:team_name]} team name"
+    end
+  end
+
+  post '/settings-change' do
+    if params[:secret] == ENV["SEAL_SECRET"]
+      team = Team.create!(name: "#{params[:team_name]}")
+      Seal.new(params[:team_name]).bark
+      "Seal received message with #{params[:team_name]} team name"
+    end
   end
 end
