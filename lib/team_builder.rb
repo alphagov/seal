613417cf28bb897fb8906692b16bd391acc28f1b
Murilo Dal Ri <murilodalri@gmail.com>
2022-09-21 10:13:18 +0100

    Remove Github teams
    This feature does not work with the new repo ownership model, it
    will keep posting to team channels about repos they don't own.

--- lib/team_builder.rb
+++ lib/team_builder.rb
@@ -38,7 +38,6 @@ private
 
   def apply_env(config)
     {
-      github_team: env["GITHUB_TEAM"] || config["github_team"],
       use_labels: env["GITHUB_USE_LABELS"] == "true" || config["use_labels"],
       compact: env["COMPACT"] == "true" || config["compact"],
       exclude_labels: env["GITHUB_EXCLUDE_LABELS"]&.split(",") || config["exclude_labels"],
