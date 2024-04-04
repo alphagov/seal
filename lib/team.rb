d49dcf26bdda8e2ad5f29c72423a6d00afd0d9b6
Murilo Dal Ri <murilodalri@gmail.com>
2024-04-04 11:33:40 +0100

    Move all configuration to a single place
    Move all team configuration from workflows into config yaml file.
    
    We also now differentiate between morning and afternoon quotes when initializing the Seal.
    That then gets checked against the teams YAML config of the same name.
    See link for example usage: https://stackoverflow.com/a/1407490

--- lib/team.rb
+++ lib/team.rb
@@ -2,6 +2,11 @@ class Team
   def initialize(
     use_labels: nil,
     security_alerts: nil,
+    morning_seal_quotes: nil,
+    seal_prs: nil,
+    afternoon_seal_quotes: nil,
+    dependapanda: nil,
+    ci_checks: nil,
     compact: nil,
     exclude_labels: nil,
     exclude_titles: nil,
@@ -12,6 +17,11 @@ class Team
   )
     @use_labels = (use_labels.nil? ? false : use_labels)
     @security_alerts = (security_alerts.nil? ? false : security_alerts)
+    @morning_seal_quotes = (morning_seal_quotes.nil? ? false : morning_seal_quotes)
+    @seal_prs = (seal_prs.nil? ? false : seal_prs)
+    @afternoon_seal_quotes = (afternoon_seal_quotes.nil? ? false : afternoon_seal_quotes)
+    @dependapanda = (dependapanda.nil? ? false : dependapanda)
+    @ci_checks = (ci_checks.nil? ? false : ci_checks)
     @compact = (compact.nil? ? false : compact)
     @quotes_days = quotes_days || []
     @exclude_labels = exclude_labels || []
@@ -25,6 +35,11 @@ class Team
     use_labels
     quotes_days
     security_alerts
+    morning_seal_quotes
+    seal_prs
+    afternoon_seal_quotes
+    dependapanda
+    ci_checks
     compact
     exclude_labels
     exclude_titles
